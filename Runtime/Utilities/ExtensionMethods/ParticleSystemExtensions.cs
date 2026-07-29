454d80e8d60e6ee3fb33baa123c8dc60cc5b3bf9
manhnd <manhnd@ikameglobal.com>
2026-07-29 22:43:56 +0700

    add
    - particle extension : stop and do st on done

---
+++ Runtime/Utilities/ExtensionMethods/ParticleSystemExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Horcrux.Runtime.Utilities.ExtensionMethods
+{
+    public static class ParticleSystemExtensions
+    {
+        /// <summary>
+        /// Stop emitting particles and wait until all remaining particles are dissolved.
+        /// </summary>
+        /// <param name="onComplete">Called when remaining particles are dissolved</param>
+        public static async UniTask StopAndAwaitCompletion(this ParticleSystem self, 
+            Action onComplete, CancellationToken ct = default)
+        {
+            if (!self) return;
+            
+            self.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            
+            while (self && self.IsAlive(true))
+                await UniTask.Yield(ct);
+
+            onComplete?.Invoke();
+        }
+    }
+}
