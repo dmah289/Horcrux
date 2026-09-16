b255ada3224ac8799ca2ec802212b2f90d8545a3
manhnd <manhnd@ikameglobal.com>
2026-09-16 17:24:09 +0700

    sync

---
+++ Runtime/Utilities/ExtensionMethods/AssetReferenceGameObjectExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Horcrux.Runtime.Utilities.ExtensionMethods
+{
+    public static class AssetReferenceGameObjectExtensions
+    {
+        public static async UniTask<T> SpawnAsync<T>(this AssetReferenceGameObject asset, Transform parent = null) where T : Component
+            => (await asset.InstantiateAsync(parent)).GetComponent<T>();
+    }
+}
