9b26a6288eb5dd1cec9e706faf0f0c8d3c56c7a4
manhnd <manhnd@ikameglobal.com>
2026-09-24 00:01:15 +0700

    sync

---
+++ Runtime/Utilities/ExtensionMethods/RectTransformExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Horcrux.Runtime.Utilities.ExtensionMethods
+{
+    public static class RectTransformExtensions
+    {
+        public static void GetLocalPosYIn(this RectTransform self, RectTransform parent, out float yMin, out float yMax)
+        {
+            Rect rect = self.rect;
+            float a = parent.InverseTransformPoint(self.TransformPoint(new Vector3(rect.xMin, rect.yMin))).y;
+            float b = parent.InverseTransformPoint(self.TransformPoint(new Vector3(rect.xMin, rect.yMax))).y;
+            
+            yMin = Mathf.Min(a, b);
+            yMax = Mathf.Max(a, b);
+        }
+    }
+}
