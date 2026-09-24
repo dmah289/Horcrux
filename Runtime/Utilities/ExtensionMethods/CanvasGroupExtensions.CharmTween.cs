c9124f567aec1f752ef47fe23fe9763214540293
manhnd <manhnd@ikameglobal.com>
2026-09-24 15:32:55 +0700

    scroll rect snap

---
+++ Runtime/Utilities/ExtensionMethods/CanvasGroupExtensions.CharmTween.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Horcrux.Runtime.Utilities.ExtensionMethods
+{
+    public static class CanvasGroupExtensions
+    {
+        public static async UniTask CharmAlpha(this CanvasGroup self, float target, float duration,
+            CancellationToken ct, float delay = 0f, Action onComplete = null)
+        {
+            float elapsed = 0;
+            float from = self.alpha;
+            float invDuration = 1.0f / Mathf.Max(duration, 0.0001f);
+            
+            try
+            {
+                if(delay > 0f)
+                    await UniTask.Delay((int)(delay * 1000), DelayType.UnscaledDeltaTime, cancellationToken: ct);
+
+                while (elapsed < duration)
+                {
+                    elapsed += Time.unscaledDeltaTime;
+                    self.alpha = Mathf.Lerp(from, target, elapsed * invDuration);
+                    await UniTask.Yield(PlayerLoopTiming.Update, ct);
+                }
+            }
+            finally
+            {
+                self.alpha = target;
+                onComplete?.Invoke();
+            }
+        }
+    }
+}
---
+++ Runtime/Utilities/ExtensionMethods/ScrollRect/ScrollRectExtensions.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Horcrux.Runtime.Utilities.ExtensionMethods
+{
+    public static class ScrollRectExtensions
+    {
+        /// <summary>
+        /// Calculate how far below the top content the viewport must be positioned
+        /// to align the itemPivot with the viewportPivot.
+        /// </summary>
+        public static void SnapVertical(this ScrollRect self, RectTransform item,
+            float itemPivot, float viewPortPivot)
+        {
+            RectTransform contentRT = self.content;
+            RectTransform viewportRT = self.viewport != null 
+                ? self.viewport : (RectTransform)self.transform;
+            
+            LayoutRebuilder.ForceRebuildLayoutImmediate(contentRT);
+            
+            float scrollableDist = contentRT.rect.height - viewportRT.rect.height;
+            if (scrollableDist <= 0f)
+            {
+                self.verticalNormalizedPosition = 1f;
+                return;
+            }
+            
+            item.GetLocalPosYIn(contentRT, out float yMin, out float yMax);
+            float itemLocalPosYInContent = Mathf.Lerp(yMin, yMax, itemPivot);
+            float topContentToItemPivotDist = contentRT.rect.yMax - itemLocalPosYInContent;
+            float pivotViewToTopViewDist = (1f - viewPortPivot) * viewportRT.rect.height;
+            float topViewToTopContentDist = topContentToItemPivotDist - pivotViewToTopViewDist;
+
+            self.verticalNormalizedPosition = 1 - Mathf.Clamp01(topViewToTopContentDist / scrollableDist);
+        }
+    }
+}
